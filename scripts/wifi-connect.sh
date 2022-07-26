e82662efcfb79304595def2f6a43747c3937f75e
btk44 <97596263+btk44@users.noreply.github.com>
2022-07-26 14:33:54 +0200

    add comments

--- scripts/wifi-connect.sh
+++ scripts/wifi-connect.sh
@@ -1,16 +1,19 @@
+#!/bin/bash
 # list wifi networks in range and connect to selected one
 
 # command listing networks
 commandOutput=$(nmcli -f BSSID,SSID,BARS,IN-USE dev wifi list | awk '{ printf "%s;",$0;}')
 
-# get each line separately
+# get each output line separately
 IFS=$';'
 lines=($commandOutput)
 unset IFS
 
-networksSSID=()
+# prepare array with network bssids
+networksBSSID=()
 i=0
 
+#display all the networks and get their bssids
 echo -e "----------------------------------"
 IFS=' '
 for line in "${lines[@]}"
@@ -19,21 +22,25 @@ do
     then
         echo -e "$i.\t${lines[$i]}"
         lineArgs=(${lines[$i]})
-	networksSSID+=(${lineArgs[0]})
+	networksBSSID+=(${lineArgs[0]}) # take bssid only
     else
-        echo -e "\t${lines[$i]}"
+        echo -e "\t${lines[$i]}" # header line
     fi
     ((i+=1))
 done
 unset IFS
 echo -e "----------------------------------"
+
+# get feedback from the user
 echo -e "select network number: "
 read selection
-selectedValue="${networksSSID[$selection-1]}"
+
+# connect to selected network
+selectedValue="${networksBSSID[$selection-1]}"
 if [ -z $selection ] || [ $selection -le 0 ] || [ -z $selectedValue ]
 then
     echo -e "nothing selected"
 else
     echo -e "your selection: ${lines[$selection]}"
-    nmcli --ask dev wifi connect "$selectedValue" 
+    nmcli --ask dev wifi connect "$selectedValue" # this will always ask for password
 fi
